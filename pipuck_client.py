04ec898faa199bb9d491ef379f9fed775f15daa1
Alan Millard <millard.alan@gmail.com>
2022-06-02 22:54:31 +0100

    Updated Pi-puck client/server to implement simple remote obstacle avoidance controller

--- pipuck_client.py
+++ pipuck_client.py
@@ -3,20 +3,124 @@
 import asyncio
 import websockets
 import json
-import random
+import signal
+
+MAX_SPEED = 500
+ir_threshold = 300
+weights_left = [-10, -10, -5, 0, 0, 5, 10, 10]
+weights_right = [-1 * x for x in weights_left]
+
+# Handle Ctrl+C termination
+
+# https://stackoverflow.com/questions/2148888/python-trap-all-signals
+SIGNALS_TO_NAMES_DICT = dict((getattr(signal, n), n) \
+    for n in dir(signal) if n.startswith('SIG') and '_' not in n )
+
+# https://github.com/aaugustin/websockets/issues/124
+__kill_now = False
+
+def __set_kill_now(signum, frame):
+    print('\nReceived signal:', SIGNALS_TO_NAMES_DICT[signum], str(signum))
+    global __kill_now
+    __kill_now = True
+
+signal.signal(signal.SIGINT, __set_kill_now)
+signal.signal(signal.SIGTERM, __set_kill_now)
+
+def kill_now() -> bool:
+    global __kill_now
+    return __kill_now
 
 async def test():
     async with websockets.connect("ws://pi-puck.local:5000") as websocket:
 
         while True:
-            data = {}
-            data["led"] = {}
-            data["led"]["num"] = random.randint(0, 2)
-            data["led"]["r"] = random.choice([True, False])
-            data["led"]["g"] = random.choice([True, False])
-            data["led"]["b"] = random.choice([True, False])
 
-            await websocket.send(json.dumps(data))
-            await asyncio.sleep(0.5)
+            # Turn of LEDs and motors when killed
+            if kill_now():
+                message = {}
+                message["set_leds_colour"] = "off"
+                message["set_outer_leds"] = [0] * 8
+                message["set_motor_speeds"] = {}
+                message["set_motor_speeds"]["left"] = 0
+                message["set_motor_speeds"]["right"] = 0
+                await websocket.send(json.dumps(message))
+                break
+
+            # Construct request for data
+            message = {}
+            message["get_ir_reflected"] = True
+            message["get_battery"] = True
+
+            # Send request for data and wait for reply
+            await websocket.send(json.dumps(message))
+            reply_json = await websocket.recv()
+            reply = json.loads(reply_json)
+
+            ir_readings = reply["ir_reflected"]
+
+            battery_charging = reply["battery"]["charging"]
+            battery_voltage = reply["battery"]["voltage"]
+            battery_percentage = reply["battery"]["percentage"]
+
+            print(ir_readings)
+            print("{}, {:.2f}V, {:.2f}%" .format("Charging" if battery_charging else "Discharging", battery_voltage, battery_percentage * 100))
+
+            # Construct command message
+            message = {}
+            message["set_outer_leds"] = [0] * 8 # e-puck body LEDs off by default (no obstacles detected)
+
+            left = right = MAX_SPEED / 2
+
+            for i, reading in enumerate(ir_readings):
+                if reading > ir_threshold:
+                    # Set wheel speeds to avoid detected obstacles
+                    left += weights_left[i] * reading
+                    right += weights_right[i] * reading
+
+                    # Illuminate e-puck body LEDs based on which IR sensors have detected an obstacle
+                    if i in [0, 7]:
+                        message["set_outer_leds"][0] = 1
+                    elif i == 1:
+                        message["set_outer_leds"][1] = 1
+                    elif i == 2:
+                        message["set_outer_leds"][2] = 1
+                    elif i == 3:
+                        message["set_outer_leds"][3] = 1
+                        message["set_outer_leds"][4] = 1
+                    elif i == 4:
+                        message["set_outer_leds"][4] = 1
+                        message["set_outer_leds"][5] = 1
+                    elif i == 5:
+                        message["set_outer_leds"][6] = 1
+                    elif i == 6:
+                        message["set_outer_leds"][7] = 1
+
+            # Set Pi-puck RGB LEDs based on whether an obstacle has been detected
+            if any(reading > ir_threshold for reading in ir_readings):
+                message["set_leds_colour"] = "red"
+            else:
+                message["set_leds_colour"] = "green"
+
+            # Clamp wheel speeds between min/max values
+            if left > MAX_SPEED:
+                left = MAX_SPEED
+            elif left < -MAX_SPEED:
+                left = -MAX_SPEED
+
+            if right > MAX_SPEED:
+                right = MAX_SPEED
+            elif right < -MAX_SPEED:
+                right = -MAX_SPEED
+
+            message["set_motor_speeds"] = {}
+            message["set_motor_speeds"]["left"] = left
+            message["set_motor_speeds"]["right"] = right
+
+            # Send command message
+            await websocket.send(json.dumps(message))
+
+            # Sleep until next control cycle
+            await asyncio.sleep(0.1)
 
 asyncio.run(test())
--- pipuck_server.py
+++ pipuck_server.py
@@ -5,23 +5,55 @@ import websockets
 import json
 from pipuck.pipuck import PiPuck
 
-pipuck = PiPuck()
+pipuck = PiPuck(epuck_version=1)
+pipuck.epuck.enable_ir_sensors(True)
 
 async def handler(websocket):
-    async for message in websocket:
-        print(message)
+    async for packet in websocket:
+        message = json.loads(packet)
 
-        data = json.loads(message)
-        print(data)
+        # Process any requests received
+        reply = {}
+        send_reply = False
 
-        if "led" in data:
-            led = data["led"]
-            num = led["num"]
-            r = led["r"]
-            g = led["g"]
-            b = led["b"]
-            print("Setting LED", num, "- R:", r, ", G:", g, ", B:", b)
-            pipuck.set_led_rgb(num, r, g, b)
+        if "get_ir_reflected" in message:
+            reply["ir_reflected"] = pipuck.epuck.ir_reflected
+            send_reply = True
+        
+        if "get_ir_ambient" in message:
+            reply["ir_ambient"] = pipuck.epuck.ir_ambient
+            send_reply = True
+
+        if "get_battery" in message:
+            charging, voltage, percentage = pipuck.get_battery_state("epuck")
+            reply["battery"] = {}
+            reply["battery"]["charging"] = charging
+            reply["battery"]["voltage"] = voltage
+            reply["battery"]["percentage"] = percentage
+            send_reply = True
+        
+        # Send reply, if requested
+        if send_reply:
+            await websocket.send(json.dumps(reply))
+
+        # Process any commands received
+        if "set_outer_leds" in message:
+            outer_leds = message["set_outer_leds"]
+            pipuck.epuck.set_outer_leds(outer_leds[0],
+                                        outer_leds[1],
+                                        outer_leds[2],
+                                        outer_leds[3],
+                                        outer_leds[4],
+                                        outer_leds[5],
+                                        outer_leds[6],
+                                        outer_leds[7])
+
+        if "set_leds_colour" in message:
+            pipuck.set_leds_colour(message["set_leds_colour"])
+
+        if "set_motor_speeds" in message:
+            pipuck.epuck.set_motor_speeds(message["set_motor_speeds"]["left"],
+                                          message["set_motor_speeds"]["right"])
 
 if __name__ == "__main__":
     start_server = websockets.serve(ws_handler=handler, host=None, port=5000)
