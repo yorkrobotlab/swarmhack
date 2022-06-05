a3279f95c5fb1ade3805c5523805fc4ad71bced4
Alan Millard <alan.millard@york.ac.uk>
2022-06-05 17:29:29 +0100

    Simplified teleop code to simply send key presses

--- teleop_client.py
+++ teleop_client.py
@@ -14,150 +14,44 @@ else:
     import tty
 
 
-class Vector3:
-    def __init(self):
-        self.x = 0
-        self.y = 0
-        self.z = 0
-
-
-class Twist:
-    def __init__(self):
-        self.linear = Vector3()
-        self.angular = Vector3()
-
-
-msg = """
-Reading from the keyboard  and Publishing to Twist!
----------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-t : up (+z)
-b : down (-z)
-anything else : stop
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-CTRL-C to quit
-"""
-
-moveBindings = {
-        'i': (1, 0, 0, 0),
-        'o': (1, 0, 0, -1),
-        'j': (0, 0, 0, 1),
-        'l': (0, 0, 0, -1),
-        'u': (1, 0, 0, 1),
-        ',': (-1, 0, 0, 0),
-        '.': (-1, 0, 0, 1),
-        'm': (-1, 0, 0, -1),
-        'O': (1, -1, 0, 0),
-        'I': (1, 0, 0, 0),
-        'J': (0, 1, 0, 0),
-        'L': (0, -1, 0, 0),
-        'U': (1, 1, 0, 0),
-        '<': (-1, 0, 0, 0),
-        '>': (-1, -1, 0, 0),
-        'M': (-1, 1, 0, 0),
-        't': (0, 0, 1, 0),
-        'b': (0, 0, -1, 0),
-    }
-
-speedBindings = {
-        'q': (1.1, 1.1),
-        'z': (.9, .9),
-        'w': (1.1, 1),
-        'x': (.9, 1),
-        'e': (1, 1.1),
-        'c': (1, .9),
-    }
-
-
-class Publisher:
-    def __init__(self, ws):
-        self.ws = ws
-
-    def publish(self, twist):
-        message = {"linear": [twist.linear.x, twist.linear.y, twist.linear.z],
-                   "angular": [twist.angular.x, twist.angular.y, twist.angular.z]}
-
-        self.ws.send(json.dumps(message))
-
-
 class PublishThread(threading.Thread):
-    def __init__(self, ws, rate):
+    def __init__(self, ws):
         super(PublishThread, self).__init__()
-        self.publisher = Publisher(ws)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.th = 0.0
-        self.speed = 0.0
-        self.turn = 0.0
+        self.ws = ws
+        self.key = ''
         self.condition = threading.Condition()
         self.done = False
 
-        # Set timeout to None if rate is 0 (causes new_message to wait forever for new data to publish)
-        if rate != 0.0:
-            self.timeout = 1.0 / rate
-        else:
-            self.timeout = None
-
         self.start()
 
-    def update(self, x, y, z, th, speed, turn):
+    def update(self, key):
         self.condition.acquire()
-        self.x = x
-        self.y = y
-        self.z = z
-        self.th = th
-        self.speed = speed
-        self.turn = turn
 
-        # Notify publish thread that we have a new message.
+        self.key = key
+
+        # Notify publish thread that we have a new message
         self.condition.notify()
         self.condition.release()
 
     def stop(self):
         self.done = True
-        self.update(0, 0, 0, 0, 0, 0)
+        self.update("teleop_exit") # Publish stop message when thread exits
         self.join()
 
     def run(self):
-        twist = Twist()
-
         while not self.done:
             self.condition.acquire()
-            # Wait for a new message or timeout
-            self.condition.wait(self.timeout)
 
-            # Copy state into twist message
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
+            # Wait for new message notification from update()
+            self.condition.wait()
+
+            key = self.key
 
             self.condition.release()
 
             # Publish
-            self.publisher.publish(twist)
-
-        # Publish stop message when thread exits
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
-        self.publisher.publish(twist)
+            message = {"key": key}
+            self.ws.send(json.dumps(message))
 
 
 def getKey(settings):
@@ -184,10 +78,6 @@ def restoreTerminalSettings(old_settings):
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
 
-def vels(speed, turn):
-    return "currently:\tspeed %s\tturn %s " % (speed, turn)
-
-
 def on_message(ws, message):
     print("Printing message: ", message)
 
@@ -202,52 +92,17 @@ def on_close(ws, close_status_code, close_msg):
 
 def on_open(ws):
     settings = saveTerminalSettings()
-
-    speed = 0.5
-    turn = 1.0
-    repeat = 0.0
-
-    pub_thread = PublishThread(ws, repeat)
-
-    x = 0.0
-    y = 0.0
-    z = 0.0
-    th = 0.0
-    status = 0.0
+    pub_thread = PublishThread(ws)
 
     try:
-        pub_thread.update(x, y, z, th, speed, turn)
-
-        print(msg)
-        print(vels(speed, turn))
-
         while True:
             key = getKey(settings)
-            if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = speed * speedBindings[key][0]
-                turn = turn * speedBindings[key][1]
 
-                print(vels(speed, turn))
-                if status == 14:
-                    print(msg)
-                status = (status + 1) % 15
-            else:
-                # Skip updating cmd_vel if key timeout and robot already stopped
-                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
-                    continue
-                x = 0
-                y = 0
-                z = 0
-                th = 0
-                if key == '\x03':
-                    break
+            # Quit if Ctrl+C is pressed
+            if key == '\x03':
+                break
 
-            pub_thread.update(x, y, z, th, speed, turn)
+            pub_thread.update(key)
 
     except Exception as e:
         print(e)
