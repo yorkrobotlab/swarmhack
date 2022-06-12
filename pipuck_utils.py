789c11463b30a532ba724e41a96023b1ba862ed1
Russell Joyce <russell@russellj.co.uk>
2022-06-12 21:08:17 +0100

    Added some basic Pi-puck management utilities

---
+++ pipuck_utils.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import robots
+
+
+def run_command(command, hide_output):
+		if hide_output:
+			process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+		else:
+			process = subprocess.Popen(command, shell=True)
+		process.wait()
+		code = process.returncode
+		if code == 0:
+			return 'OK'
+		else:
+			return 'ERROR'
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Pi-puck management utilities.')
+	parser.add_argument('utility', choices=['ping', 'ssh-copy-id', 'copy-server', 'shutdown'])
+
+	args = parser.parse_args()
+	if args.utility == 'ping':
+		for (robot_id, ip) in robots.pipucks.items():
+			if ip != '':
+				result = run_command(f'ping -c2 -t3 {ip}', True)
+				print(f'{robot_id}: {result}')
+	elif args.utility == 'ssh-copy-id':
+		for (robot_id, ip) in robots.pipucks.items():
+			if ip != '':
+				result = run_command(f'ssh-copy-id -o StrictHostKeyChecking=no pi@{ip}', False)
+				print(f'{robot_id}: {result}')
+	elif args.utility == 'copy-server':
+		for (robot_id, ip) in robots.pipucks.items():
+			if ip != '':
+				result = run_command(f'scp pipuck_server.py pi@{ip}:', False)
+				print(f'{robot_id}: {result}')
+	elif args.utility == 'shutdown':
+		for (robot_id, ip) in robots.pipucks.items():
+			if ip != '':
+				result = run_command(f'ssh pi@{ip} sudo poweroff', False)
+				print(f'{robot_id}: {result}')
