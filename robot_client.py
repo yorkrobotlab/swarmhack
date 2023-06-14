e0ac8355a7947a21b019e4db493036864de3853c
Ian Gray <iangray001@github.com>
2023-06-14 17:13:47 +0100

    Restructure and document client

--- robot_client.py
+++ robot_client.py
@@ -14,74 +14,186 @@ import random
 import inspect
 from vector2d import Vector2D
 import math
+import pprint
 import angles
-
 import colorama
 from colorama import Fore
 
-colorama.init(autoreset=True)
+"""
+This function is the main loop of your application. You can make any changes you want throughout this 
+file, but most of your game logic will be located in here.
+
+First, ensure that the robot_ids list below is correctly set to the robots you wish to work with.
+For example:
+    robot_ids = [34, 37, 39]
+
+When run, this script will connect to the server and then repeatedly call the main_loop() function.
+The script communicates with the robots using a websockets connection. Sending data requires that you 
+use Python's asynchronous I/O. If you are not familiar with this, the rule is to remember that the control
+function should be declared with "async" (see the simple_obstacle_avoidance() example) and called from 
+main_loop() using loop.run_until_complete(async_thing_to_run(ids))
+"""
+
+active_robots = {}
+ids = []
+robot_ids = [34, 37, 39]
+
+def main_loop():
+    # This line requests all robot virtual sensor data from the tracking server for the robots specified in robot_ids
+    # This is stored in active_robots, a map of id -> instances of the Robot class (defined lower in this file) 
+    print(Fore.GREEN + "[INFO]: Requesting data from tracking server")
+    loop.run_until_complete(get_server_data())
+
+    # Request sensor data from detected robots
+    # This augments the Robot instances with their battery level and the values from each robot's proximity sensors
+    # You only need to do this if you care about their battery level, or are using their proximity sensors
+    print(Fore.GREEN + "[INFO]: Robots detected:", ids)
+    print(Fore.GREEN + "[INFO]: Requesting data from detected robots")
+    loop.run_until_complete(get_robot_data(ids))
+
+    # Now run our behaviour
+    print(Fore.GREEN + "[INFO]: Sending commands to detected robots")
+    loop.run_until_complete(send_robot_commands(ids))
+
+    print()
+
+    # Sleep until next control cycle. We use 0.1 seconds by default so as to not flood the network.
+    time.sleep(0.1)
+
+"""
+This is an example of a behaviour. You will want to replace this with a behaviour that implements your team
+movements. It currently is an example of basic object avoidance.
+"""
+async def send_commands(robot):
+    try:
+        # Turn off LEDs and motors when killed. Please remember to do this!
+        if kill_now():
+            message = {"set_leds_colour": "off", "set_motor_speeds": {}}
+            message["set_motor_speeds"]["left"] = 0
+            message["set_motor_speeds"]["right"] = 0
+            await robot.connection.send(json.dumps(message))
+
+        message = {}
+
+        """
+        Construct a command message
+        Robots are controlled by sending them a json dictionary which we create here using the message variable
+        
+        We can set the speed of the wheel motors (from -255 to 255). Setting them to the same value makes the robot go forwards
+        or backwards. Setting them differently makes the robot turn. For example:
+        message["set_motor_speeds"]["left"] = 100
+        message["set_motor_speeds"]["right"] = 100
+    
+        We can also set the colour of the LED. Supported colours are "off", "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+        message["set_leds_colour"] = "green"
+
+        You can combine commands (i.e. setting both wheels and the LED colour in one go)
+
+        The rest of this function is an example object avoidance behaviour which goes FORWARD unless the IR sensor
+        detects something in front of it, when it will turn instead.
+        """
+        if robot.state == RobotState.FORWARDS:
+            left = right = robot.MAX_SPEED
+            if (time.time() - robot.turn_time > 0.5) and any(ir > robot.ir_threshold for ir in robot.ir_readings):
+                robot.turn_time = time.time()
+                robot.state = random.choice((RobotState.LEFT, RobotState.RIGHT))
+            elif (time.time() - robot.regroup_time > 5):
+                robot.regroup_time = time.time()
+                robot.state = RobotState.REGROUP
+
+        elif robot.state == RobotState.BACKWARDS:
+            left = right = -robot.MAX_SPEED
+            robot.turn_time = time.time()
+            robot.state = RobotState.FORWARDS
+
+        elif robot.state == RobotState.LEFT:
+            left = -robot.MAX_SPEED
+            right = robot.MAX_SPEED
+            if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
+                robot.turn_time = time.time()
+                robot.state = RobotState.FORWARDS
+
+        elif robot.state == RobotState.RIGHT:
+            left = robot.MAX_SPEED
+            right = -robot.MAX_SPEED
+            if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
+                robot.turn_time = time.time()
+                robot.state = RobotState.FORWARDS
+
+        elif robot.state == RobotState.STOP:
+            left = right = 0
+            robot.turn_time = time.time()
+            robot.state = RobotState.FORWARDS
+
+        elif robot.state == RobotState.REGROUP:
+            message["set_leds_colour"] = "green"
+            direction = Vector2D(0, 0)
+            for neighbour_id, neighbour in robot.neighbours.items():
+                direction += Vector2D(neighbour["range"] * math.cos(math.radians(neighbour["bearing"])),
+                                      neighbour["range"] * math.sin(math.radians(neighbour["bearing"])))
+            direction_polar = direction.to_polar()
+            heading = angles.normalize(math.degrees(direction_polar[1]), -180, 180)
+            if heading > 0:
+                left = robot.MAX_SPEED
+                right = 0
+            else:
+                left = 0
+                right = robot.MAX_SPEED
+            if time.time() - robot.regroup_time > random.uniform(3.0, 4.0):
+                message["set_leds_colour"] = "red"
+                robot.state = RobotState.FORWARDS
+
+        message["set_motor_speeds"] = {}
+        message["set_motor_speeds"]["left"] = left
+        message["set_motor_speeds"]["right"] = right
+
+        # Send command message
+        await robot.connection.send(json.dumps(message))
+
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
 
 
-##
-# Replace `server_none` with one of `server_york`, `server_sheffield`, or `server_manchester` here,
-#  or specify a custom server IP address as a string.
-# All ports should remain at 80.
-##
+
+# Server address, port details, globals
+#---------------------------------------
 server_address = server_york
 server_port = 6000
 robot_port = 6000
-##
 
 if len(server_address) == 0:
     raise Exception(f"Enter local tracking server address on line {inspect.currentframe().f_lineno - 6}, "
                     f"then re-run this script.")
 
+server_connection = None
+colorama.init(autoreset=True)
 
-# Persistent Websockets!!!!!!!!!!!!!!!!
-# https://stackoverflow.com/questions/59182741/python-websockets-lib-client-persistent-connection-with-class-implementation
 
-
-##
 # Handle Ctrl+C termination
 # https://stackoverflow.com/questions/2148888/python-trap-all-signals
-
+#---------------------------------------------------------------------
 SIGNALS_TO_NAMES_DICT = dict((getattr(signal, n), n) \
     for n in dir(signal) if n.startswith('SIG') and '_' not in n)
-
 # https://github.com/aaugustin/websockets/issues/124
 __kill_now = False
-
 
 def __set_kill_now(signum, frame):
     print('\nReceived signal:', SIGNALS_TO_NAMES_DICT[signum], str(signum))
     global __kill_now
     __kill_now = True
 
-
 signal.signal(signal.SIGINT, __set_kill_now)
 signal.signal(signal.SIGTERM, __set_kill_now)
-
 
 def kill_now() -> bool:
     global __kill_now
     return __kill_now
 
-# Ctrl+C termination handled
-##
 
-# class Ball():
-#     def __init__(self, position):
-#         self.position = position
+# Robot class and structures
+#----------------------------
 
-
-
-server_connection = None
-active_robots = {}
-ids = []
-# ball = Ball([0, 0])
-
-
-# Robot states to use in the controller
+# Robot states to use in the example controller
 class RobotState(Enum):
     FORWARDS = 1
     BACKWARDS = 2
@@ -89,7 +201,6 @@ class RobotState(Enum):
     RIGHT = 4
     STOP = 5
     REGROUP = 6
-
 
 # Main Robot class to keep track of robot states
 class Robot:
@@ -224,6 +335,8 @@ async def get_server_data():
         reply_json = await server_connection.recv()
         reply = json.loads(reply_json)
 
+        pprint.PrettyPrinter(indent=4).pprint(reply)
+
         # ball.position = reply["ball"]["position"]
         # print(ball.position)
         # del reply['ball']
@@ -289,94 +402,6 @@ async def get_data(robot):
         print(f"{type(e).__name__}: {e}")
 
 
-# Send motor and LED commands to robot
-# This function also performs the obstacle avoidance state machine
-async def send_commands(robot):
-    try:
-        # Turn off LEDs and motors when killed
-        if kill_now():
-            message = {"set_leds_colour": "off", "set_motor_speeds": {}}
-            message["set_motor_speeds"]["left"] = 0
-            message["set_motor_speeds"]["right"] = 0
-            await robot.connection.send(json.dumps(message))
-
-        # Construct command message
-        message = {}
-
-        print("ROBOT STATE:", robot.state)
-
-        if robot.state == RobotState.FORWARDS:
-            left = right = robot.MAX_SPEED
-
-            if (time.time() - robot.turn_time > 0.5) and any(ir > robot.ir_threshold for ir in robot.ir_readings):
-                robot.turn_time = time.time()
-                robot.state = random.choice((RobotState.LEFT, RobotState.RIGHT))
-            elif (time.time() - robot.regroup_time > 5):
-                robot.regroup_time = time.time()
-                robot.state = RobotState.REGROUP
-
-        elif robot.state == RobotState.BACKWARDS:
-            left = right = -robot.MAX_SPEED
-            robot.turn_time = time.time()
-            robot.state = RobotState.FORWARDS
-
-        elif robot.state == RobotState.LEFT:
-            left = -robot.MAX_SPEED
-            right = robot.MAX_SPEED
-
-            if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
-                robot.turn_time = time.time()
-                robot.state = RobotState.FORWARDS
-
-        elif robot.state == RobotState.RIGHT:
-            left = robot.MAX_SPEED
-            right = -robot.MAX_SPEED
-
-            if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
-                robot.turn_time = time.time()
-                robot.state = RobotState.FORWARDS
-
-        elif robot.state == RobotState.STOP:
-            left = right = 0
-            robot.turn_time = time.time()
-            robot.state = RobotState.FORWARDS
-
-        elif robot.state == RobotState.REGROUP:
-            message["set_leds_colour"] = "green"
-            direction = Vector2D(0, 0)
-            for neighbour_id, neighbour in robot.neighbours.items():
-                direction += Vector2D(neighbour["range"] * math.cos(math.radians(neighbour["bearing"])),
-                                      neighbour["range"] * math.sin(math.radians(neighbour["bearing"])))
-            direction_polar = direction.to_polar()
-            heading = angles.normalize(math.degrees(direction_polar[1]), -180, 180)
-            print("heading:", heading)
-            if heading > 0:
-                left = robot.MAX_SPEED
-                right = 0
-            else:
-                left = 0
-                right = robot.MAX_SPEED
-
-            if time.time() - robot.regroup_time > random.uniform(3.0, 4.0):
-                message["set_leds_colour"] = "red"
-                robot.state = RobotState.FORWARDS
-
-        message["set_motor_speeds"] = {}
-        message["set_motor_speeds"]["left"] = left
-        message["set_motor_speeds"]["right"] = right
-
-        # Set RGB LEDs based on battery voltage
-        # if robot.battery_voltage < robot.BAT_LOW_VOLTAGE:
-        #     message["set_leds_colour"] = "red"
-        # else:
-        #     message["set_leds_colour"] = "green"
-
-        # Send command message
-        await robot.connection.send(json.dumps(message))
-
-    except Exception as e:
-        print(f"{type(e).__name__}: {e}")
-
 
 # Main entry point for robot control client sample code
 if __name__ == "__main__":
@@ -388,13 +413,7 @@ if __name__ == "__main__":
         print(Fore.RED + "[ERROR]: No connection to server")
         sys.exit(1)
 
-    # Specify robot IDs to work with here. For example for robots 11-15 use:
-    #  robot_ids = range(11, 16)
-    robot_ids = [34, 37, 39]
-
-    if len(robot_ids) == 0:
-        raise Exception(f"Enter range of robot IDs to control on line {inspect.currentframe().f_lineno - 3}, "
-                        f"tleft = right = robot.MAX_SPEEDhen re-run this script.")
+    assert len(robot_ids) > 0
 
     # Create Robot objects
     for robot_id in robot_ids:
@@ -413,25 +432,8 @@ if __name__ == "__main__":
 
     # Only communicate with robots that were successfully connected to
     while True:
-        # Request all robot virtual sensor data from the tracking server
-        print(Fore.GREEN + "[INFO]: Requesting data from tracking server")
-        loop.run_until_complete(get_server_data())
+        main_loop()
 
-        # Request sensor data from detected robots
-        print(Fore.GREEN + "[INFO]: Robots detected:", ids)
-        print(Fore.GREEN + "[INFO]: Requesting data from detected robots")
-        loop.run_until_complete(get_robot_data(ids))
-
-        # Calculate next step of control algorithm, and send commands to robots
-        print(Fore.GREEN + "[INFO]: Sending commands to detected robots")
-        loop.run_until_complete(send_robot_commands(ids))
-
-        print()
-
-        # TODO: Close websocket connections
         if kill_now():
             loop.run_until_complete(stop_robots(robot_ids))  # Kill all robots, even if not visible
             break
-
-        # Sleep until next control cycle
-        time.sleep(0.1)
